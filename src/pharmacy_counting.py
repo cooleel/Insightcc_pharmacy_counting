f61a7843af9522fa9f14a394fa8c198e7ca37c6e
Shanshan Wang <wangss523@gatech.edu>
2018-07-17 20:55:47 -0500

    dic_structure

--- src/pharmacy_counting.py
+++ src/pharmacy_counting.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 """
+Created on Tue Jul 17 20:42:18 2018
+
+@author: Shanshan
+"""
+# -*- coding: utf-8 -*-
+"""
 Created on Mon Jul 16 10:37:58 2018
 
 @author: Shanshan
@@ -9,68 +15,69 @@ Created on Mon Jul 16 10:37:58 2018
 import csv
 import sys
 
-#open file
-    
-def get_data(file):
-    #read in txt file line by line,separate header and the body text
-    with open(file,'r') as f_in:
+#open file and read in data
+
+def get_data(inputFile):
+    #read in txt file,separate header and the body text
+    with open(inputFile,'r') as f_in:
+        next(f_in) # header line
         f_input = csv.reader(f_in)
-        next(f_input)
-       # header = next(f_input)
-       #0: id, 1: last name, 2: first name, 3: drug name, 4: cost
         return list(f_input)
 
 def process_count(data):
-    #process the counting, return aggregated list with each durg is a single entry
-    #along with the number of prescirbers and total cost
-    f_output = []
-    temp_drug = []
-    temp_prescriber = []
-    temp_num_prescriber = []
-    temp_cost = []
+    count_dic = {}    # store counting data to a dictionary
     
     for line in data:
-        #0: id, 1: last name, 2: first name, 3: drug name, 4: cost
-        prescriber_inline = line[1]+line[2]
-#        print(line)
-#        print(f_output)
-        if line[3] == temp_drug:
-            temp_cost += float(line[4])
-            if prescriber_inline != temp_prescriber:
-                temp_num_prescriber +=1
+        prescriber_inline = line[1].lower() + line[2].lower()
+        drug_name = line[3]
+        drug_cost = float(line[4])
+
+        if drug_name not in count_dic:            
+            unique_prescriber = set([prescriber_inline])
+            total_cost = drug_cost
+            count_dic[drug_name] = [unique_prescriber, total_cost]
         else:
-            
-            if temp_drug:
-                f_output.append([temp_drug,temp_num_prescriber,round(temp_cost)])
-              #  f_output.append('\n')
-               
-            temp_drug = line[3]
-            temp_cost = float(line[4])
-            temp_prescriber = prescriber_inline
-            temp_num_prescriber = 1
-    f_output.append([temp_drug,temp_num_prescriber,round(temp_cost)])
-    f_output.sort(key = lambda x:(-x[2],x[0]))
-    return f_output
+            exist_drug = count_dic[drug_name]
+            exist_drug[0].add(prescriber_inline)
+            exist_drug[1]+= drug_cost
+    #    print(data)
+    return count_dic
     
+
+def process_data(result):
+    tuples = []
+    
+    for drug in result:
+
+        drug_info = result[drug]
+        num_prescriber = len(drug_info[0])
+        total_cost = drug_info[1]
+        tuples.append((drug, num_prescriber, round(total_cost)))
+        
+    tuples.sort(key = lambda x:(-x[2],x[0]))
+
+    return tuples
+            
 def write_output(file,result,output_header):
-    with open(file, 'w',newline='') as f_out:
+    with open(file, 'w') as f_out:
         f_ouput = csv.writer(f_out)
         f_ouput.writerows(output_header)
         f_ouput.writerows(result)
     
 def main():
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
-#    input_file = 'itcont.txt'
-#    output_file = 'top_cost_drug.txt'
+#    input_file = sys.argv[1]
+#    output_file = sys.argv[2]
+    input_file = 'itcont.txt'
+    output_file = 'top_cost_drug.txt'
+    #output_header follows the instuction
     output_header = [['drug_name','num_prescriber','total_cost']]
     
     data = get_data(input_file)
     result = process_count(data)
-    write_output(output_file,result,output_header)
-    print(data)
-    print(result)
-        
-    
+    output_text = process_data(result)
+    write_output(output_file,output_text,output_header)
+ #   print(data)
+ #   print(result)
+           
 if __name__ == '__main__':
     main()
